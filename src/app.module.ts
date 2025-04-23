1f07f4a7871015bb29ae903d3242c6afcc76608f
MJ <wkdaudwn11@naver.com>
2025-04-23 17:46:02 +0900

    chore: 환경 파일 구조 정리 및 cross-env 기반 실행 환경 분리

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HealthModule } from './health/health.module';
 
 @Module({
-  imports: [HealthModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: [`.env.${process.env.NODE_ENV}`, '.env'],
+    }),
+    HealthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,8 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
+    console.log('process.env.NODE_ENV', process.env.NODE_ENV);
+    console.log('process.env.PORT', process.env.PORT);
     return 'Hello World!';
   }
 }
